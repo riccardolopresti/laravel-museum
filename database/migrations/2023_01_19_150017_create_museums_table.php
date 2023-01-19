cb9a485314880bbfd06b56f24b89e65d948d7ef6
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2023-01-19 16:48:16 +0100

    add migration

--- database/migrations/2023_01_19_150017_create_museums_table.php
+++ database/migrations/2023_01_19_150017_create_museums_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('museums', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 60);
+            $table->string('slug', 70)->unique();
+            $table->string('nation', 30);
             $table->timestamps();
         });
     }
