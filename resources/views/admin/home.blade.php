9e25b8d60e6d7e3223c3ca185f0b7342ed148658
RiccardoLoPresti <riccardolopresti35@gmail.com>
2023-01-19 17:59:37 +0100

    [add] route museums

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -6,7 +6,7 @@
             <a href="{{route('admin.artists.index')}}">Artist</a>
         </li>
         <li>
-            <a href="#">Museum</a>
+            <a href="{{route('admin.museums.index')}}">Museum</a>
         </li>
     </ul>
 @endsection
