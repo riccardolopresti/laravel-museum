80f54e06ffa0e16a0c98c82195287687178416a0
MarBozzz <mbozzuto@libero.it>
2023-01-19 17:10:11 +0100

    [added] seeders

--- database/seeders/ArtistSeeder.php
+++ database/seeders/ArtistSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Artist;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,13 @@ class ArtistSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $array_artists = config('paintings.artists');
+
+        foreach ($array_artists as $artist) {
+            $new_artist = new Artist();
+            $new_artist->name = $artist['name'];
+            $new_artist->slug = Artist::generateSlug($new_artist->name);
+            $new_artist->save();
+        }
     }
 }
