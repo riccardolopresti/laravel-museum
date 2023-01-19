e7c1768e78c4ccb97cf6c7da4d1011603031e59a
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2023-01-19 16:58:53 +0100

    add seeder

--- app/Models/Museum.php
+++ app/Models/Museum.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Museum extends Model
 {
     use HasFactory;
+
+    public static function generateSlug($string) {
+        $slug = Str::slug($string, '-');
+        $original_slug = $slug;
+
+        $existing = Museum::where('slug', $slug)->first();
+        $c = 1;
+        while($existing) {
+            $slug = $original_slug . '-' . $c;
+            $existing = Museum::where('slug', $slug)->first();
+            $c++;
+        };
+        return $slug;
+    }
 }
